cd9670984e9f8391f6d8446d7b262c9d36dfaf82
Manu <manu@endurance-game.com>
2015-11-07 04:47:06 +0100

    ADDED Readme file

---
+++ README.md
@@ -0,0 +1,10 @@
+Basic Node Chat
+
+=====================
+
+
+Two possibilities for server distribution:
+
+1. Use complete package on Node server via "node server.js".
+
+2. Put folder "content" on Apache or any other HTTP server and the rest on a Node server. Comment out the express section in the server.js.
--- server.js
+++ server.js
@@ -2,12 +2,20 @@ var express = require('express');
 var app = express();
 var io = require('socket.io');
 
+
+//Sending index.html with Express
 app.use(express.static('/content/'));
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/content/index.html');
 });
 
+
+//Socket.io connection
+
+
+
+//Starting server
 app.listen(8000, function() {
 	console.log('Server running on localhost:8000');
 });

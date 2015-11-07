292f684f29915df626379a81b3230bc6703aed96
Manu <manu@endurance-game.com>
2015-11-07 04:59:41 +0100

    FIXED content paths

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+var http = require('http');
 var express = require('express');
 var app = express();
-var io = require('socket.io');
+var io = require('socket.io')(http);
 
 
 //Sending index.html with Express
-app.use(express.static('/content/'));
+app.use(express.static(__dirname + '/content/'));
+//app.use(express.static(__dirname + '/content/css/'));
+//app.use(express.static(__dirname + '/content/js/'));
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/content/index.html');
@@ -12,7 +15,9 @@ app.get('/', function(req, res) {
 
 
 //Socket.io connection
+io.on('connection', function(socket){
 
+});
 
 
 //Starting server

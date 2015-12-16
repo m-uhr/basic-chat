ec8df8437aa7a47fdd317b1edc9954ad81422fe3
muhr <manuela.uhr@gmx.net>
2015-12-16 07:39:19 +0100

    REFACTOR

--- public/js/main.js
+++ public/js/main.js
@@ -1,61 +1,134 @@
 var socket = io();
 
 var windowHeight = $( window ).height();
-
+var KEYCODE_ENTER = 13;
 
 var ownNickname = prompt("Your nickname:", "Guest");
 
-$(".own-chat-message").keydown(function(event)
-{
-	if( event.which == 13 ) 
-	{
+var ChatWindow = new ChatWindow();
 
-		var ownMessage = $(".own-chat-message").val();
 
-		if( ownMessage || ownMessage.length !== 0 )
-		{
 
-			$( ".chat-window-messages" ).append( newMessageHTML(ownNickname, ownMessage, 'own-message') ); //Add to chat window
-			$( ".own-chat-message" ).val( "" ); //Empty message box
+/* Sending own message */
+$( ".own-chat-message" ).keydown( function( event ) {
 
-			socket.emit( 'newMessage', { username: ownNickname, message: ownMessage}); //Send to server
+	if( event.which == KEYCODE_ENTER ) {
 
-			//Add scrollbar, if too many messages
-			if( windowHeight - $( ".own-chat-bar" ).height() <= $( ".chat-window .chat-window-messages" ).height() )
-			{
-				$( ".chat-window" ).height( windowHeight - $( ".own-chat-bar" ).height() );
-				$( ".chat-window" ).css( "overflow-y", "scroll" );
-			}
-		}
-		
+		ChatWindow.addOwnMessage( $(".own-chat-message").val() );		
 	} 
 
-	else 
-	{
+	else {
+
 		console.log("Writing message...");
 	}
 });
 
 
-socket.on('spreadNewMessage', function (msg) 
-{
-	console.log("asdfasdf:    "+ msg.username +" MESSAGE " + ownNickname);
-	if( msg.username !== ownNickname )
-	{
-		$(".chat-window-messages").append( newMessageHTML(msg.username, msg.message) );
-	}
-		
+/* Getting other messages */
+socket.on('spreadNewMessage', function( msg ) {
+
+	ChatWindow.addNewMessage( msg );	
 });
 
-function newMessageHTML (username, message, cssClass='') 
-{
-	return '<li class="'+ cssClass +'"><span class="chat-username">'+ username +'</span>'
-		+'>'
-		+'<span class="chat-message">'+ message + '</span></li>';
-}
+
+
+/* Render View */
+var RenderHtml = {
+
+	spacer : '>',
+	classForOwnMessages : 'own-message',
+
+	newMessage : function( username, message, isOwnMessage=false ) 
+		{
+			var cssClass = '';
+			if( isOwnMessage ) cssClass = this.classForOwnMessages;
+
+			return '<li class="'+ cssClass +'">'
+						+ this.wrapUsername(username, isOwnMessage) 
+						+ this.spacer 
+						+ this.wrapMessage(message, isOwnMessage) 
+					+ '</li>';
+		},
+	anotherOwnMessage : function ( message, isOwnMessage=false ) 
+		{
+			return wrapMessage( message, isOwnMessage );
+		},
+
+	wrapUsername : function ( username, isOwnMessage=false ) 
+		{
+			var cssClass = '';
+			if( isOwnMessage ) cssClass = this.classForOwnMessages;
+
+			if( username ) return '<span class="chat-username '+ cssClass +'">'+ username +'</span>';
+		},
+
+	wrapMessage : function ( message, isOwnMessage=false ) 
+		{
+			var cssClass = '';
+			if( isOwnMessage ) cssClass = this.classForOwnMessages;
+
+			if( message ) return '<span class="chat-message '+ cssClass +'">'+ message + '</span>';
+		}
+
+};
+
+
+/* Chat window class */
+function ChatWindow() {
+
+	this.messagesHeight = $( ".chat-window .chat-window-messages" ).height();
+	this.hasScrollbar = false;
+
+	this.addOwnMessage = function( ownMessage='' ) {
+		if( ownMessage || ownMessage.length !== 0 ) {
+
+			$( ".chat-window-messages" ).append( RenderHtml.newMessage(ownNickname, ownMessage, true) ); //Add to chat window
+			$( ".own-chat-message" ).val( "" ); //Empty message box
+
+			socket.emit( 'newMessage', { username: ownNickname, message: ownMessage}); //Send to server
+
+			this.updateMessagesHeight();
+		}
+	};
+
+	this.addNewMessage = function( data ) {
+
+		console.log("asdfasdf:    "+ data.username +" MESSAGE " + ownNickname);
+
+		if( data.username !== ownNickname ){
+
+			$(".chat-window-messages").append( RenderHtml.newMessage(data.username, data.message) );
+
+			this.updateMessagesHeight();
+		}
+	};
+
+	this.updateMessagesHeight = function() {
+
+		this.messagesHeight = $( ".chat-window .chat-window-messages" ).height();
+
+		if( !this.hasScrollbar 
+			&& this.messagesHeight >= windowHeight - $( ".own-chat-bar" ).height() ) {
+
+			this.addScrollbar();
+		}
+
+	};
+
+	this.addScrollbar = function() {
+
+		if( $( ".chat-window" ).css( "overflow-y") !== "scroll" ) {
+	
+			$( ".chat-window" ).height( windowHeight - $( ".own-chat-bar" ).height() );
+			$( ".chat-window" ).css( "overflow-y", "scroll" );	
+
+			this.hasScrollbar = true;
+		}
+	};
+
+	this.getLastMessage = function() {};
+};
 
 
 
-$(window).resize(function() {
-  //update stuff
-});
+
